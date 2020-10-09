7255176b6b5ba142b2d750a0801bef55cd6cad65
alvarovalerocom <72454968+alvarovalerocom@users.noreply.github.com>
2020-10-09 15:48:00 +0200

    Getting user input

---
+++ primenumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class primenumber {
+	public static void main(String[] args){
+		//code
+		Scanner scanner = new
+		Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int myNumber = scanner.nextInt();
+	}
+}
