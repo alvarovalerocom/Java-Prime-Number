baf73158f65438f25e6a9c38c68b62b0e5cac80e
alvarovalerocom <72454968+alvarovalerocom@users.noreply.github.com>
2020-10-09 16:01:57 +0200

    Last commit
    -General code improvement.
    -Added the base functionallity of the program.

--- primenumber.java
+++ primenumber.java
@@ -3,10 +3,23 @@ import java.util.*;
 
 public class primenumber {
 	public static void main(String[] args){
+		
+		System.out.println("Enter a number: ");
 		//code
-		Scanner scanner = new
-		Scanner(System.in);
-		System.out.print("Enter a number: ");
-		int myNumber = scanner.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		int userInput = scanner.nextInt();
+		scanner.close();
+
+		//maths
+		int potentialFactor = 2;
+		while (userInput % potentialFactor != 0) {
+			potentialFactor ++;
+		}
+		if (potentialFactor == userInput) {
+			System.out.println("The number is prime.");
+		}
+		else {
+			System.out.println("The number isn't prime.");
+		}
 	}
 }
